ff30a3e5eadc76fedcb24c14cbe993eb7b419171
Khaled Hammouda <khaledh@gmail.com>
2024-05-26 17:28:52 -0400

    gfxsrv: use double-buffering

--- src/kernel/gfxsrv.nim
+++ src/kernel/gfxsrv.nim
@@ -5,6 +5,8 @@
 import drivers/bga
 import taskmgr
 
+{.experimental: "codeReordering".}
+
 let
   logger = DebugLogger(name: "gfxsrv")
 
@@ -16,6 +18,9 @@ const
 
 var
   fb: ptr UncheckedArray[uint32]
+  buffer0: ptr UncheckedArray[uint32]
+  buffer1: ptr UncheckedArray[uint32]
+  backBuffer: ptr UncheckedArray[uint32]
 
 proc start*()  {.cdecl.} =
   logger.info "starting graphics server"
@@ -23,9 +28,25 @@ proc start*()  {.cdecl.} =
   bga.setResolution(XRes, YRes)
   fb = bga.getFramebuffer()
 
-  # clear the framebuffer
+  buffer0 = fb
+  buffer1 = fb +! uint64(XRes * YRes * 4)
+  backBuffer = buffer1
+
+  # set the back buffer to the default background color
   for i in 0 ..< YRes.uint32 * Xres.uint32:
-    fb[i] = DefaultBackground
+    backBuffer[i] = DefaultBackground
+
+  swapBuffers()
 
   # suspend ourselves for now
   suspend()
+
+proc swapBuffers*() =
+  if backBuffer == buffer0:
+    bgaSetYOffset(0)
+    backBuffer = buffer1
+    copyMem(buffer1, buffer0, XRes * YRes * 4)
+  else:
+    bgaSetYOffset(YRes)
+    backBuffer = buffer0
+    copyMem(buffer0, buffer1, XRes * YRes * 4)
