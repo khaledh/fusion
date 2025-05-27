493220151867c4b552b2638aae284126ebfdae37
Khaled Hammouda <khaledh@gmail.com>
2025-05-27 00:26:10 -0400

    kbd: add ps/2 keyboard driver and wire it to the console

--- src/kernel/con/console.nim
+++ src/kernel/con/console.nim
@@ -7,6 +7,7 @@ import channels
 import framebuffer as fb
 import font
 import sched
+import drivers/kbd
 
 let
   logger = DebugLogger(name: "console")
@@ -20,11 +21,33 @@ var
   curRow, curCol = 0  # cursor position
   leftPadding, rightPadding = 6
   topPadding, bottomPadding = 6
-  curFont = dina13x7
+  curFont = dina15x7
 
-proc moveCursor*(x, y: int) =
+# Forward declarations
+proc putChar*(row, col: int, c: char, color: uint32 = ForegroundColor)
+
+proc moveTo*(x, y: int) =
   curCol = x
   curRow = y
+
+proc nextLine*() =
+  curCol = 0
+  curRow += 1
+  if curRow >= maxRows:
+    curRow = 0  # TODO: scroll up
+
+proc nextCol*() =
+  curCol += 1
+  if curCol >= maxCols:
+    nextLine()
+
+proc backspace*() =
+  if curCol > 0:
+    dec curCol
+  elif curRow > 0:
+    dec curRow
+    curCol = maxCols - 1
+  putChar(curRow, curCol, ' ', BackgroundColor)
 
 proc cur2px*(row, col: int): (int, int) =
   (leftPadding + col * curFont.width, topPadding + row * curFont.height)
@@ -36,6 +59,8 @@ proc putChar*(row, col: int, c: char, color: uint32 = ForegroundColor) =
     for j in 0 ..< curFont.width:
       if (glyph[i] shr (8 - j - 1) and 1) == 1:
         fb.putPixel(x + j, y + i, color)
+      else:
+        fb.putPixel(x + j, y + i, BackgroundColor)
 
 proc putString*(row, col: int, s: string, color: uint32 = ForegroundColor): (int, int) =
   var (r, c) = (row, col)
@@ -51,15 +76,31 @@ proc putString*(row, col: int, s: string, color: uint32 = ForegroundColor): (int
 proc write*(s: string, color: uint32 = ForegroundColor) =
   (curRow, curCol) = putString(curRow, curCol, s, color)
 
+proc keyEventHandler(keyEvent: KeyEvent) =
+  # logger.info &"received keyEvent: {keyEvent}"
+  if keyEvent.eventType == KeyDown:
+    case keyEvent.ch
+      of '\t': moveTo(curCol + 4, curRow)
+      of '\n': nextLine()
+      of '\b': backspace()
+      else:
+        putChar(curRow, curCol, keyEvent.ch, ForegroundColor)
+        nextCol()
+
 proc start*(chid: int) {.cdecl.} =
   ## Start the console task and wait for messages from the channel.
 
   # initialize the framebuffer
+  logger.info "init framebuffer"
   fb.init()
   fb.clear(BackgroundColor)
   maxCols = (fb.getWidth() - leftPadding - rightPadding) div curFont.width
   maxRows = (fb.getHeight() - topPadding - bottomPadding) div curFont.height
   logger.info &"console size: {maxCols} x {maxRows}"
+
+  # initialize the keyboard
+  logger.info "init keyboard"
+  kbd.kbdInit(keyEventHandler)
 
   # create a channel to receive messages for display
   let cid = channels.create[FString](getCurrentTask(), mode = ChannelMode.Read)
@@ -70,4 +111,4 @@ proc start*(chid: int) {.cdecl.} =
     if msgOpt.isSome:
       write(msgOpt.get)
     else:
-      logger.info &"console: received `none` from channel {cid}"
+      logger.info &"received `none` from channel {cid}"
---
+++ src/kernel/drivers/kbd.nim
@@ -0,0 +1,143 @@
+#[
+  PS/2 keyboard driver.
+]#
+import std/strformat
+
+import ports
+import kernel/idt
+import kernel/ioapic
+import kernel/lapic
+
+let
+  logger = DebugLogger(name: "kbd")
+
+const
+  KbdInterruptVector = 0x21'u8
+
+  kbdUs = [
+    '\0', '\x1B', '1',  '2',  '3',  '4',  '5',  '6',  # 00-07
+    '7',  '8',    '9',  '0',  '-',  '=',  '\b', '\t', # 08-0f
+    'q',  'w',    'e',  'r',  't',  'y',  'u',  'i',  # 10-17
+    'o',  'p',    '[',  ']',  '\n', '\0', 'a',  's',  # 18-1f
+    'd',  'f',    'g',  'h',  'j',  'k',  'l',  ';',  # 20-27
+    '\'', '`',    '\0', '\\', 'z',  'x',  'c',  'v',  # 28-2f
+    'b',  'n',    'm',  ',',  '.',  '/',  '\0', '*',  # 30-37
+    '\0', ' ',    '\0', '\0', '\0', '\0', '\0', '\0', # 38-3f
+    '\0', '\0',   '\0', '\0', '\0', '\0', '\0', '7',  # 40-47
+    '8',  '9',    '-',  '4',  '5',  '6',  '+',  '1',  # 48-4f
+    '2',  '3',    '0',  '.',  '\0', '\0', '\0', '\0', # 50-57
+    '\0',                                             # 58-5f
+  ]
+  kbdUsShift = [
+    '\0', '\x1B', '!',  '@',  '#',  '$',  '%',  '^',  # 00-07
+    '&',  '*',  '(',  ')',    '_',  '+',  '\b', '\t', # 08-0f
+    'Q',  'W',  'E',  'R',    'T',  'Y',  'U',  'I',  # 10-17
+    'O',  'P',  '{',  '}',    '\n', '\0', 'A',  'S',  # 18-1f
+    'D',  'F',  'G',  'H',    'J',  'K',  'L',  ':',  # 20-27
+    '"',  '~',  '\0', '|',    'Z',  'X',  'C',  'V',  # 28-2f
+    'B',  'N',  'M',  '<',    '>',  '?'
+  ]
+
+type
+  KeyEventType* = enum
+    KeyDown = (0, "KeyDown")
+    KeyUp = (1, "KeyUp")
+  KeyEvent* = object
+    eventType*: KeyEventType
+    scanCode: uint8
+    ch*: char
+    shift*: bool
+    ctrl*: bool
+    alt*: bool
+  KeyEventHandler* = proc (evt: KeyEvent)
+
+var
+  shift, ctrl, alt = false
+  handleKeyEvent: KeyEventHandler
+
+proc `$`*(evt: KeyEvent): string =
+  result.add($evt.eventType)
+  if evt.ch == '\0':
+    result.add("(NUL)")
+  elif evt.ch == '\x1B':
+    result.add("(ESC)")
+  elif evt.ch == '\n':
+    result.add("(LF)")
+  elif evt.ch == '\t':
+    result.add("(TAB)")
+  elif evt.ch == '\b':
+    result.add("(BS)")
+  elif evt.ch == '\r':
+    result.add("(CR)")
+  elif evt.ch == ' ':
+    result.add("(SP)")
+  elif evt.scanCode == 0x48:
+    result.add("(UP)")
+  elif evt.scanCode == 0x50:
+    result.add("(DOWN)")
+  elif evt.scanCode == 0x4b:
+    result.add("(LEFT)")
+  elif evt.scanCode == 0x4d:
+    result.add("(RIGHT)")
+  else:
+    result.add("('" & $evt.ch & "')")
+  result.add(&" scanCode={evt.scanCode:0>2x}h")
+  if evt.shift: result.add(" shift")
+  if evt.ctrl: result.add(" ctrl")
+  if evt.alt: result.add(" alt")
+
+proc kbdInterruptHandler*(intFrame: ptr InterruptFrame)
+    {.cdecl, codegenDecl: "__attribute__ ((interrupt)) $# $#$#".}=
+
+  var scanCode = portIn8(0x60)
+  # logger.info &"scanCode = {scanCode:0>2x}h"
+
+  if (scanCode and 0x80) == 0:
+    # key press down
+    case scanCode
+      of 0x2a, 0x36: shift = true
+      of 0x1d: ctrl = true
+      of 0x38: alt = true
+      else:
+        var ch: char = '\0'
+        if shift and scanCode < kbdUsShift.len:
+          ch = kbdUsShift[scanCode]
+        elif scanCode < kbdUs.len:
+          ch = kbdUs[scanCode]
+        let keyEvent = KeyEvent(
+          eventType: KeyDown, ch: ch, scanCode: scanCode,
+          shift: shift, ctrl: ctrl, alt: alt
+        )
+        # logger.info "keyEvent = ", $keyEvent
+        if not isNil(handleKeyEvent):
+          handleKeyEvent(keyEvent)
+
+  else:
+    # key release
+    scanCode = scanCode and (not 0x80'u8)
+    case scanCode
+      of 0x2a, 0x36: shift = false
+      of 0x1d: ctrl = false
+      of 0x38: alt = false
+      else:
+        var ch: char = '\0'
+        if shift and scanCode < kbdUsShift.len:
+          ch = kbdUsShift[scanCode]
+        elif scanCode < kbdUs.len:
+          ch = kbdUs[scanCode]
+        let keyEvent = KeyEvent(
+          eventType: KeyUp, ch: ch, scanCode: scanCode,
+          shift: shift, ctrl: ctrl, alt: alt
+        )
+        # logger.info "keyEvent = ", $keyEvent
+        if not isNil(handleKeyEvent):
+          handleKeyEvent(keyEvent)
+
+  lapic.eoi()
+
+proc kbdInit*(handler: KeyEventHandler) =
+  handleKeyEvent = handler
+  logger.info &"Setting keyboard interrupt handler ({KbdInterruptVector:0>2x}h)"
+  idt.installHandler(KbdInterruptVector, kbdInterruptHandler)
+  # set keyboard interrupt: interrupt input 1 => vector 21h
+  ioapic.setRedirEntry(irq = 1, vector = KbdInterruptVector)
